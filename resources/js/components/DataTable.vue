fc2e811e54f047a611701d621fd42c3876f8f508
mc0918 <michael.campbell@northwestern.edu>
2025-05-30 00:19:12 -0500

    reactive search

--- resources/js/components/DataTable.vue
+++ resources/js/components/DataTable.vue
@@ -1,28 +1,57 @@
 <script>
+    import debounce from 'lodash/debounce';
+
     export default {
         props: {
             countries: Object,
             fields: Object
         },
-        setup(props) {
-            console.log(props.countries);
-        }
+        data() {
+            return {
+                query: '',
+            };
+        },
+        methods: {
+            filterCountries() {
+                return this.countries.filter(country => {
+                    if (country.name.toLowerCase().includes(this.query.toLowerCase())) {
+                        return country;
+                    }
+                });
+            },
+            debouncedSearch: debounce(function () {
+                this.filterCountries();
+            }, 300)
+        },
+
     }
 </script>
 
 <template>
+    <div class="searchBar">
+        <div class="input-group">
+            <input type="search" class="form-control" v-model="query" @input="debouncedSearch" placeholder="Search by Country Name">
+        </div>
+    </div>
     <table id="tableComponent" class="table table-bordered table-striped">
         <thead>
             <tr>
+                <th>Save Country</th>
                 <th v-for="field in fields" :key='field'>
-                    {{ field }}
+                    {{ field.charAt(0).toUpperCase() + field.slice(1).toLowerCase() }}
                 </th>
             </tr>
         </thead>
         <tbody>
-        <tr v-for="country in countries" :key="country.name">
+        <tr v-for="country in filterCountries()" :key="country.name">
+            <td>checkbox</td>
             <td v-for="field in fields">
-                {{country[field]}}
+                <div v-if="typeof country[field] === 'boolean'">
+                    {{ country[field] ? "yes" : "no"}}
+                </div>
+                <div v-else>
+                    {{country[field]}}
+                </div>
             </td>
         </tr>
 
